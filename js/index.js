30d141e2a1f1bcc1dd9e1a0c8143f6722655a61c
rutamt <95180713+rutamt@users.noreply.github.com>
2023-06-27 08:27:00 -0400

    Changed contribution

--- js/index.js
+++ js/index.js
@@ -219,10 +219,10 @@ function onMapClick(e) {
 
 map.on("click", onMapClick);
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
-    attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    maxZoom: 18,
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20
 }).addTo(map);
 
 function calculateScore() {
